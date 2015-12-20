5e9d3275ff589ba67d8e2c07c5229e767f24904d
Ted Kalaw <ted.kalaw@gmail.com>
2015-12-19 18:27:40 -0800

    Adds frequency enum

---
+++ Chainlink/Frequency.swift
@@ -0,0 +1,13 @@
+//
+//  Frequency.swift
+//  Chainlink
+//
+//  Created by Ted Kalaw on 12/19/15.
+//  Copyright © 2015 Ted Kalaw. All rights reserved.
+//
+
+import Foundation
+
+enum Frequency: String {
+  case DAILY = "daily"
+}

a3e9ba3370c4bcf9dd51836f5539586808d7ba78
Ted Kalaw <ted.kalaw@gmail.com>
2015-12-20 10:56:56 -0800

    Adds SwipeCounterView and swiping a view in a table cell to reveal one below

--- Chainlink/ChainViewCell.swift
+++ Chainlink/ChainViewCell.swift
@@ -28,6 +28,8 @@ class ChainViewCell: UITableViewCell {
   var delegate:ChainViewCellDelegate?
   var originalCenter:CGPoint = CGPoint()
   var shouldIncrement:Bool = false
+  var settingsButton:UIButton?
+  var myContentView:UITableViewCell!
 
   required init(coder aDecoder: NSCoder) {
     fatalError("NSCoding not supported")
@@ -40,17 +42,20 @@ class ChainViewCell: UITableViewCell {
     self.layoutMargins = UIEdgeInsetsZero
     self.selectionStyle = .None
 
-    self.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
-
     self.chainTitleLabel = UILabel()
     self.linkCountLabel = UILabel()
 
-    self.contentView.addSubview(self.chainTitleLabel!)
-    self.contentView.addSubview(self.linkCountLabel!)
+    self.myContentView = UITableViewCell()
+    self.myContentView!.addSubview(self.chainTitleLabel!)
+    self.myContentView!.addSubview(self.linkCountLabel!)
+    self.myContentView!.backgroundColor = UIColor.redColor()
+    self.myContentView!.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
 
     let recognizer = UIPanGestureRecognizer(target: self, action: "handlePan:")
     recognizer.delegate = self
+    self.contentView.addSubview(self.myContentView!)
     self.contentView.addGestureRecognizer(recognizer)
+    self.contentView.backgroundColor = UIColor.blackColor()
   }
 
   // TODO lol figure out a non-shitty way to layout frames
@@ -59,12 +64,13 @@ class ChainViewCell: UITableViewCell {
     self.contentView.layoutIfNeeded()
     self.chainTitleLabel!.frame = CGRectMake(20, 12, 200, 20)
     self.linkCountLabel!.frame = CGRectMake(330, 12, 30, 20)
+    self.myContentView!.frame = self.bounds
   }
 
   func handlePan(recognizer: UIPanGestureRecognizer) {
     if recognizer.state == .Began {
       // when the gesture begins, record the current center location
-      self.originalCenter = center
+      self.originalCenter = self.myContentView!.center
     }
 
     if (recognizer.state == .Changed) {
@@ -75,19 +81,18 @@ class ChainViewCell: UITableViewCell {
         return
       }
 
-      center = CGPointMake(originalCenter.x + translation.x, originalCenter.y)
+      self.myContentView!.center = CGPointMake(originalCenter.x + translation.x, originalCenter.y)
       // has the user dragged the item far enough
-      self.shouldIncrement = frame.origin.x > frame.size.width / 8.0
+      self.shouldIncrement = self.myContentView!.frame.origin.x > frame.size.width / 8.0
     }
 
     if (recognizer.state == .Ended) {
-      let originalFrame = CGRect(x: 0, y: frame.origin.y,
-        width: bounds.size.width, height: bounds.size.height)
+      let originalFrame = self.bounds
       if (shouldIncrement) {
         self.addLink()
       }
       // snap back to original location
-      UIView.animateWithDuration(0.2, animations: {self.frame = originalFrame})
+      UIView.animateWithDuration(0.2, animations: {self.myContentView!.frame = originalFrame})
       self.shouldIncrement = false
     }
   }
---
+++ Chainlink/SwipeCounterView.swift
@@ -0,0 +1,14 @@
+//
+//  SwipeCounterView.swift
+//  Chainlink
+//
+//  Created by Ted Kalaw on 12/20/15.
+//  Copyright © 2015 Ted Kalaw. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SwipeCounterView : UIView {
+
+}

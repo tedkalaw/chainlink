81e9f9743545c6ebe01044782d1e74b3e7b9a2d2
Ted Kalaw <ted.kalaw@gmail.com>
2015-12-23 11:46:38 -0800

    Adds swipe left to reveal second view

--- Chainlink/ChainViewCell.swift
+++ Chainlink/ChainViewCell.swift
@@ -41,6 +41,8 @@ class ChainViewCell: UITableViewCell {
   var optionsView:SwipeOptionView!
   var activeView:ActiveView
 
+  var isRightViewOpen:Bool = false
+
   required init(coder aDecoder: NSCoder) {
     fatalError("NSCoding not supported")
   }
@@ -116,11 +118,15 @@ class ChainViewCell: UITableViewCell {
   }
 
   func handleTap(recognizer: UITapGestureRecognizer) {
-    self.delegate?.handleSelectChain(self.chain!)
+    if (self.isRightViewOpen) {
+      self.closeRightView()
+    } else {
+      self.delegate?.handleSelectChain(self.chain!)
+    }
   }
 
   override func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer: UIGestureRecognizer) -> Bool {
-    return true
+    return false
   }
 
   override func gestureRecognizerShouldBegin(gestureRecognizer: UIGestureRecognizer) -> Bool {
@@ -145,6 +151,10 @@ class ChainViewCell: UITableViewCell {
    In this view, swiping right is used to quickly add a link
    */
   private func handleSwipeRightChanged(recognizer: UIPanGestureRecognizer) -> Void {
+    // TODO: handle what happens if you swipe right while the options thing is open
+    self.optionsView.hidden = true
+    self.counterView.hidden = false
+
     let translation = recognizer.translationInView(self)
 
     let swipeDistance:CGFloat = frame.size.width / 5.0
@@ -189,10 +199,43 @@ class ChainViewCell: UITableViewCell {
   }
 
   private func handleLeftSwipeChanged(recognizer: UIPanGestureRecognizer) -> Void {
+    self.counterView.hidden = true
+    self.optionsView.hidden = false
 
+    if (self.isRightViewOpen) {
+      return
+    }
+
+    let maxSwipeDistance = frame.size.width / 5.0
+    let translation = recognizer.translationInView(self)
+    if (fabs(translation.x) < maxSwipeDistance) {
+      self.myContentView!.center = CGPointMake(originalCenter.x + translation.x, originalCenter.y)
+    } else {
+      self.isRightViewOpen = true
+    }
+
+
+    // TODO: handle manual close swipe
   }
 
   private func handleLeftSwipeEnded() -> Void {
+    if (!self.isRightViewOpen) {
+      self.closeRightView()
+    }
+  }
 
+  private func closeRightView() -> Void {
+    UIView.animateWithDuration(
+      0.2,
+      animations: {
+        self.myContentView!.frame = self.bounds
+      },
+      completion: {
+        (finished: Bool) in
+        if (finished) {
+          self.isRightViewOpen = false
+        }
+      }
+    )
   }
 }

400de79dd5487b707f51d1f992f4b8c9b663226c
Ted Kalaw <ted.kalaw@gmail.com>
2015-12-29 17:44:31 -0800

    Adds frequency and dayBitmap fields to Chain to use for settings

--- Chainlink/Chain+CoreDataProperties.swift
+++ Chainlink/Chain+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  Chain+CoreDataProperties.swift
 //  Chainlink
 //
-//  Created by Ted Kalaw on 12/28/15.
+//  Created by Ted Kalaw on 12/29/15.
 //  Copyright © 2015 Ted Kalaw. All rights reserved.
 //
 //  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
@@ -12,16 +12,15 @@
 import Foundation
 import CoreData
 
-import Foundation
-import CoreData
-
 extension Chain {
 
-  @NSManaged var name: String?
   @NSManaged var consecutiveLinks: NSNumber?
+  @NSManaged var name: String?
+  @NSManaged var frequency: NSNumber?
+  @NSManaged var dayBitmap: NSNumber?
   @NSManaged var links: NSSet?
   @NSManaged var target: Target?
-
+  
   @NSManaged func addLinksObject(link: Link)
   @NSManaged func removeLinksObject(link: Link)
   @NSManaged func addEmployees(links: NSSet)
